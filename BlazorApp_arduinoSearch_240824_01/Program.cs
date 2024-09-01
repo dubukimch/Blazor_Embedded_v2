338ae12f488326866dd6addf6b3215e3fc22ca05
rakrak <166417831+Rakorean@users.noreply.github.com>
2024-09-01 18:16:18 +0900

    Server와 WebAssembly 융합 테스트
    1. Unity WebGL 빌드 후 웹어셈블리 프로젝트의 wwwroot에 위치
    2. Blazor server에서 새로 추가로 프로젝트 생성

--- BlazorApp_arduinoSearch_240824_01/Program.cs
+++ BlazorApp_arduinoSearch_240824_01/Program.cs
@@ -1,14 +1,12 @@
-using BlazorApp_arduinoSearch_240824_01.Data;
-using BlazorApp_arduinoSearch_240824_01.Pages;
-using BlazorApp_arduinoSearch_240824_01.Services;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
-using System.Text.Json;
-
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+using BlazorApp_arduinoSearch_240824_01.Data;
+using BlazorApp_arduinoSearch_240824_01.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
@@ -17,6 +15,7 @@ builder.Services.AddSingleton<WeatherForecastService>();
 // DeviceDiscoveryService 등록
 builder.Services.AddHttpClient<DeviceDiscoveryService>();
 builder.Services.AddSingleton<MqttService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,8 +28,22 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
+// MIME 유형 등록
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings.Remove(".unityweb");
+provider.Mappings.Add(".unityweb", "application/octet-stream");
+provider.Mappings.Remove(".data");
+provider.Mappings.Add(".data", "application/octet-stream");
+provider.Mappings.Remove(".wasm");
+provider.Mappings.Add(".wasm", "application/wasm");
+provider.Mappings.Remove(".symbols.json");
+provider.Mappings.Add(".symbols.json", "application/octet-stream");
 
+//app.UseStaticFiles(new StaticFileOptions
+//{
+//    ContentTypeProvider = provider
+//});
+app.UseStaticFiles(); // 이 줄이 있는지 확인
 app.UseRouting();
 
 app.MapBlazorHub();
