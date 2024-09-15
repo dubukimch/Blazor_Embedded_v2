e64997068dd6a1f6e7a526035462aab3552bf7e6
origin <101459905+dubukimch@users.noreply.github.com>
2024-09-15 10:03:52 +0900

    Blazor의 장비 연결 페이지 MatBlazor적용 #10
    1. Topic을 트리노드로 표현
    2. Description 글자 수 넘으면 생략 표현 및 ToolTip 추가(개선의 여지 필요)
    3. 버튼 MatBlazor 적용

--- BlazorApp_arduinoSearch_240824_01/wwwroot/js/lightSensorChart.js
+++ BlazorApp_arduinoSearch_240824_01/wwwroot/js/lightSensorChart.js
@@ -1,38 +1,39 @@
 window.renderLightSensorChart = (elementId, data) => {
     var ctx = document.getElementById(elementId).getContext('2d');
     window.lightSensorChart = new Chart(ctx, {
-        type: 'line',  // 'line' or 'bar' for line chart or bar chart
+        type: 'bar',  // 'bar' for bar chart
         data: {
             labels: ['East', 'West', 'South', 'North'],  // X축 레이블
             datasets: [
                 {
                     label: 'Morning',
                     data: data.morning.map(x => parseInt(x, 10)),  // parseInt를 통해 숫자로 변환
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 2,
-                    fill: false
+                    backgroundColor: 'rgba(75, 192, 192, 1)',  // 막대 색상
+                    borderWidth: 1
                 },
                 {
                     label: 'Afternoon',
                     data: data.afternoon.map(x => parseInt(x, 10)),
-                    borderColor: 'rgba(153, 102, 255, 1)',
-                    borderWidth: 2,
-                    fill: false
+                    backgroundColor: 'rgba(153, 102, 255, 1)',  // 막대 색상
+                    borderWidth: 1
                 },
                 {
                     label: 'Evening',
                     data: data.evening.map(x => parseInt(x, 10)),
-                    borderColor: 'rgba(255, 159, 64, 1)',
-                    borderWidth: 2,
-                    fill: false
+                    backgroundColor: 'rgba(255, 159, 64, 1)',  // 막대 색상
+                    borderWidth: 1
                 }
             ]
         },
         options: {
             responsive: true,
             scales: {
+                x: {
+                    stacked: true  // X축에 대해 스택을 활성화
+                },
                 y: {
-                    beginAtZero: false,  // 데이터가 너무 작지 않도록 Y축을 0에서 시작하지 않게 설정
+                    stacked: true,  // Y축에 대해 스택을 활성화
+                    beginAtZero: true,  // Y축을 0에서 시작
                     ticks: {
                         callback: function (value) {
                             return value.toString();  // Y축에 표시될 값 포맷팅
---
+++ BlazorApp_arduinoSearch_240824_01/wwwroot/js/site.js
@@ -0,0 +1,4 @@
+﻿function matBlazorInitFunction() {
+    console.log("MatBlazor initialized.");
+    // 필요한 초기화 작업을 여기에 추가
+}
