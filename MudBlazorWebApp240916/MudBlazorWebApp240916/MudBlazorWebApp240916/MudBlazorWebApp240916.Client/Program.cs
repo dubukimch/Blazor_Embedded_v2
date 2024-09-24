ae0f9fc2818942fdcac3f90debdde381add9da46
rakrak <101459905+dubukimch@users.noreply.github.com>
2024-09-25 02:44:11 +0900

    Devices.razor 페이지 수정 #12
    1. 연결 가능과 등록 장비 목록 이동으로 수정
    2. 아두이노 연결 가능에서는 mqtt 보여주고 등록 장비에서는 제어창 버튼으로 전환

--- MudBlazorWebApp240916/MudBlazorWebApp240916/MudBlazorWebApp240916/MudBlazorWebApp240916.Client/Program.cs
+++ MudBlazorWebApp240916/MudBlazorWebApp240916/MudBlazorWebApp240916/MudBlazorWebApp240916.Client/Program.cs
@@ -7,5 +7,8 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddMudServices();
 builder.Services.AddScoped<MqttService>();  // 여기서 MqttService를 등록
 builder.Services.AddScoped<DeviceDiscoveryService>(); // Register DeviceDiscoveryService to DI container
-
+builder.Services.AddHttpClient("MyHttpClient", client =>
+{
+    client.BaseAddress = new Uri("http://172.30.1.12:5044"); // 예: http://192.168.1.100:5000/
+});
 await builder.Build().RunAsync();
