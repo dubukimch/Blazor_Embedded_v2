7afe62cf7870ea69332ac3fe17689a4bb270ea63
rakrak <101459905+dubukimch@users.noreply.github.com>
2024-09-30 03:50:27 +0900

    차트 기능 추가 #14

--- BlazorChart240928/BlazorChart240928/wwwroot/js/chartScripts.js
+++ BlazorChart240928/BlazorChart240928/wwwroot/js/chartScripts.js
@@ -1,47 +1,59 @@
-﻿function getRandomColor() {
+﻿function renderChart(chartType, xAxis, datasets) {
+    if (typeof window.myChart !== 'undefined' && window.myChart !== null && typeof window.myChart.destroy === 'function') {
+        window.myChart.destroy();
+    }
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    var chartConfig = {
+        type: chartType === 'stackedBar' || chartType === 'barHorizontal' ? 'bar' : chartType,
+        data: {
+            labels: xAxis,
+            datasets: datasets.map(d => ({
+                label: d.label,
+                data: d.data,
+                backgroundColor: getRandomColor() + '33',
+                borderColor: getRandomColor(),
+                borderWidth: 1
+            }))
+        },
+        options: {
+            scales: {
+                x: {
+                    stacked: chartType === 'stackedBar'
+                },
+                y: {
+                    beginAtZero: true,
+                    stacked: chartType === 'stackedBar'
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: true
+        }
+    };
+
+    if (chartType === 'barHorizontal') {
+        chartConfig.options.indexAxis = 'y';
+    }
+
+    if (chartType === 'pie' || chartType === 'doughnut') {
+        chartConfig.data = {
+            labels: xAxis,
+            datasets: [{
+                data: datasets[0].data,
+                backgroundColor: datasets[0].data.map(() => getRandomColor())
+            }]
+        };
+    }
+
+    window.myChart = new Chart(ctx, chartConfig);
+}
+
+function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-}
-
-function renderChart(chartType, xAxis, yAxis) {
-    if (window.myChart) {
-        if (typeof window.myChart.destroy === "function") {
-            window.myChart.destroy(); // 이전 차트가 있으면 삭제
-        }
-    }
-
-    // Generate random colors for each data point
-    var backgroundColors = [];
-    var borderColors = [];
-    for (var i = 0; i < yAxis.length; i++) {
-        var randomColor = getRandomColor();
-        backgroundColors.push(randomColor + '33'); // 투명도 조정
-        borderColors.push(randomColor);
-    }
-
-    var ctx = document.getElementById('myChart').getContext('2d');
-    window.myChart = new Chart(ctx, {
-        type: chartType,
-        data: {
-            labels: xAxis,
-            datasets: [{
-                label: '차트 데이터',
-                data: yAxis,
-                backgroundColor: backgroundColors,
-                borderColor: borderColors,
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
 }
