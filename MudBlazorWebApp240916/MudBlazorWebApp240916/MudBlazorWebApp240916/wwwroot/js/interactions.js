0b7a3b6086bfa0b22639060c5804c6ca4c35c2fd
origin <101459905+dubukimch@users.noreply.github.com>
2024-10-10 02:44:57 +0900

    불필요한 js 삭제

--- MudBlazorWebApp240916/MudBlazorWebApp240916/MudBlazorWebApp240916/wwwroot/js/interactions.js
+++
@@ -1,9 +0,0 @@
-﻿function invokeBlazorDialog(dialogType) {
-    // Blazor의 메서드를 호출하며 dialogType에 따라 처리
-    DotNet.invokeMethodAsync('MudBlazorWebApp240916.Client', 'ShowBlazorDialog', dialogType)
-        .then(function () {
-            console.log("Blazor dialog opened: " + dialogType);
-        }).catch(function (error) {
-            console.error("Error invoking Blazor method:", error);
-        });
-}
